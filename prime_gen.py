d43806d67b72cc6a51ec2335349a8a982e0ee725
Swapnil Raj <raj.swapnil99@gmail.com>
2017-12-16 23:01:03 +0000

    Util functions for prime numbers involved in the algorithm

---
+++ prime_gen.py
@@ -0,0 +1,16 @@
+from sympy import randprime, isprime
+
+def get_min_max_bit_size(bit):
+    return (int(('1' + '0' * (bit - 1)), 2), int(('1' * bit), 2))
+
+def get_prime_p():
+    range_start, range_end = get_min_max_bit_size(1024)
+    return randprime(range_start, range_end)
+
+def get_prime_g():
+    range_start, range_end = get_min_max_bit_size(4096)
+    return randprime(range_start, range_end)
+
+def get_prime_private():
+    range_start, range_end = get_min_max_bit_size(256)
+    return randprime(range_start, range_end)
