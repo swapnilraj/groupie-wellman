9dccae3205d3d6b4c0c89f07cfe1e8e7e11a0385
Jorik Schellekens <JorikSch@gmail.com>
2017-12-17 18:50:16 +0000

    Byte comparison fix.

--- util.py
+++ util.py
@@ -4,7 +4,7 @@ def send_ACK(conn):
     conn.sendall(_ACK)
 
 def get_ACK(conn):
-    return conn.recv(1024).equals(_ACK)
+    return conn.recv(1024) == _ACK
 
 def compute_power(base, power, mod):
     return pow(base, power, mod)
