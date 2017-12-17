d35e4aef703b6144e611b0462eb65991e697073d
Jorik Schellekens <JorikSch@gmail.com>
2017-12-17 18:50:43 +0000

    Client logic implemented.

--- cage2.py
+++ cage2.py
@@ -1,2 +1,35 @@
-import numpy
-import    
+import prime_gen
+import socket
+
+from util import *
+
+_g_SIZE = 4096
+
+def connect(host, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+
+        # Wait for connection acknowledge. Not sure this is necessary. Does s.connect() thread blcok?
+        get_ACK(s)
+
+        # compute b
+        b = prime_gen.get_prime_private()
+
+        # get p and g
+        p = int.from_bytes(s.recv(_g_SIZE), byteorder='big', signed=False)
+        send_ACK(s)
+
+        g = int.from_bytes(s.recv(_g_SIZE), byteorder='big', signed=False)
+        print(p)
+        send_ACK(s)
+
+        # get pA
+        pa = int.from_bytes(s.recv(_g_SIZE), byteorder='big', signed=False)
+        send_ACK(s)
+
+        # send pB
+        s.sendall(compute_power(p, b, g).to_bytes(_g_SIZE, byteorder='big'))
+        get_ACK(s)
+
+        return compute_power(pa, b, g)
+
